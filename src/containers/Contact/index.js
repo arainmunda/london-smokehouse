763ecfb1086cc8fc40d4af71be6bd00afdf8752c
arainmunda <awaesishfaq@gmail.com>
2017-10-30 19:19:19 +0000

    Contact, faqs, mobile footer
    - Finish contact page
    - Complete FAQs page
    - Ensure footer works in mobile

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -12,6 +12,7 @@ export default function Contact () {
             <div className="row no-gutters-xs">
                 <div className="col-xs-12 col-sm-4">
                     <h3>London Smokehouse</h3>
+                    <p>We are available for bookings at food markets, restaurant pop ups, events, birthday parties and any other celebrations.</p>
                     <p>
                         <a href="mailto:info@londonsmokehouse.uk">info@londonsmokehouse.uk</a><br />
                         020 7426 5103
--- src/containers/Faqs/index.js
+++ src/containers/Faqs/index.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import './Faqs.css'
 
 export default function Faqs () {
     return (
-        <div>FAQs</div>
+        <div className="content-page faqs container-fluid">
+            <h1>
+                <span className="header-border"></span>
+                <span className="title">FAQs</span>
+                <span className="header-border"></span>
+            </h1>
+            
+            <h3>Do you have any allergen information?</h3>
+            <ul>
+                <li>Our foods may contain a trace of nuts.</li>
+                <li>Our coleslaw, mac & cheese and turkey all contain lactose.</li>
+                <li>Our duck contains soya.</li>
+                <li>Our cornbread and mac & cheese contains gluten</li>
+                <li>Always check with us and explain your dietary and allergen requirements before purchasing</li>
+            </ul>
+
+            <h3>Is your food suitable for vegetarians?</h3>
+            <p>Erm... probably not. We are working on a vegetarian recipes, but they're not quite there yet.</p>
+
+            <h3>Is your meat halal?</h3>
+            <p>YES, all of our meat is halal, including our sausage which is definitely not made of pork (they're made of a blend of beef, lamb and veal depending on availability). Our beer can chicken uses the non-alcholic variation.</p>
+
+            <h3>Do you have any nutritional information</h3>
+            <p>Because of the nature of how we make our food, using seasonal vegetables, different breeds of meat, with different fat content, it's incredibly difficult for us to supply this information. If you're having red meat at London Smokehouse, treat us like a cheat day.</p>
+
+            <h3>I have leftovers, can I eat them?</h3>
+            <p>Our food is cooked fresh on the day, so if you refridgerate your meal from London Smokehouse, then it should keep for a few days. Cold smoking does preserve food, but we cold smoke so don't recommend keeping your food too long.</p>
+        </div>
     )
 }
