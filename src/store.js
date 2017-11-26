88e67f19f7e15fcdf84e02baa18fb1ae3026e1a6
arainmunda <awaesishfaq@gmail.com>
2017-11-26 11:08:32 +0000

    Routing & Tracking
    - Fix Routing
    - Enable GA and FBQ

--- src/store.js
+++ src/store.js
@@ -1,14 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux'
-import createBrowserHistory from 'history/createBrowserHistory'
+import { routerMiddleware } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
+export const history = createHistory()
 const initialState = {}
-const browserHistory = createBrowserHistory()
-const middleware = [thunk, routerMiddleware(browserHistory)]
+const middleware = [thunk, routerMiddleware(history)]
 const enhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(rootReducer, initialState, enhancers(applyMiddleware(...middleware)))
 
-export const history = syncHistoryWithStore(browserHistory, store)
 export default store
